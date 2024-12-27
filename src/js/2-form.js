96a182aa915c5e708b208fdb68442421eac12042
balo-max <shoni6124@gmail.com>
2024-12-27 13:17:00 +0100

    add task-2

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+const feedbackForm = document.querySelector('.feedback-form'); 
+
+let formData = {
+    email: "",
+    message: "",
+};
+
+const fillFormFields = () => {
+    try {
+        if (localStorage.length === 0) {
+            return;
+        }
+
+        const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+        formData = formDataFromLS;
+
+        for (const key in formDataFromLS) {
+            feedbackForm.elements[key].value = formDataFromLS[key];
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+fillFormFields();
+
+const onFormFieldChange = event => {
+    const { target: formFieldEl } = event;
+
+    const fieldValue = formFieldEl.value;
+    const fieldName = formFieldEl.name;
+
+    formData[fieldName] = fieldValue;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+};
+
+const onFeedbackFormSubmit = event => {
+    event.preventDefault();
+
+    const { currentTarget: formEl } = event;
+
+     if (!formEl.elements.email.value || !formEl.elements.message.value) {
+         alert('Fill please all fields');
+         return;
+    }
+
+    formEl.reset(); 
+    localStorage.removeItem('feedback-form-state');
+}
+
+feedbackForm.addEventListener('input', onFormFieldChange);
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
